1f5651e0d4ef82cb9ce71e03fe7dbb83db58176a
Aslam-16 <54734988+Aslam-16@users.noreply.github.com>
2020-12-22 10:49:07 +0530

    Update searchbuyer.js

--- src/components/searchbuyer.js
+++ src/components/searchbuyer.js
@@ -40,7 +40,7 @@ class SearchBuyer extends React.Component{
 
 	componentDidMount(){
 		const option=this;
-		axios.get('https://d50ed78b4f8a.ngrok.io/api/v1/buyers/listbuyers')
+		axios.get('https://a4e182c50fd7.ngrok.io/api/v1/buyers/listbuyers')
         .then((response) => {
 	     option.setState({data:response.data.data,newdata:response.data.data})
   });
